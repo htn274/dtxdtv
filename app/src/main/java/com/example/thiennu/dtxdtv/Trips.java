de7b7b517f9599e4c7badef406ee1c5b091d658e
bao <baosws@gmail.com>
2018-12-20 18:33:12 +0700

    added sign up layout

--- app/src/main/java/com/example/thiennu/dtxdtv/Trips.java
+++ app/src/main/java/com/example/thiennu/dtxdtv/Trips.java
@@ -3,6 +3,7 @@ package com.example.thiennu.dtxdtv;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -19,12 +20,14 @@ public class Trips extends AppCompatActivity {
         setContentView(R.layout.activity_trips);
         tripList = new ArrayList<>();
         tripList.add(new TripInfo("Trip to Hue"));
+        Log.d("btag", "sadfasg");
         lvTrips = (ListView) findViewById(R.id.lvTrips);
         TripListAdapter customAdaper = new TripListAdapter(this,R.layout.trip_info, tripList);
         lvTrips.setAdapter(customAdaper);
         lvTrips.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("btag", "fck");
                 Intent intent = new Intent(getApplicationContext(), groupTrip.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/thiennu/dtxdtv/login.java
+++ app/src/main/java/com/example/thiennu/dtxdtv/login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
@@ -11,6 +12,13 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ((Button)findViewById(R.id.buttonSignUp)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void loginOnlick(View view) {
